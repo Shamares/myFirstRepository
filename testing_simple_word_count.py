61837974822568c62a9a74f67b798565a6364602
Shamares <mkipisz1999@gmail.com>
2024-06-12 10:23:04 +0200

    added input possibilities

--- testing_simple_word_count.py
+++ testing_simple_word_count.py
@@ -1,9 +1,6 @@
 # simple testing word count in a string
-
-string = 'Szeth-son-son-Vallano, Truthless of Shinovar, wore white on the day he was to kill a king. The white ' \
-         'clothing was a Parshendi tradition, foreign to him. But he did as his masters required and did not ask for ' \
-         'an explanation.'
-
+print('Enter your text')
+string = input()
 words = string.split()
 total = 0
 for words in words:
