aa68902a64669df612d428fdeb8ec770470fde1e
Shamares <mkipisz1999@gmail.com>
2024-06-23 20:41:17 +0200

    added loop for multiple uses and exit message

--- testing_simple_word_count.py
+++ testing_simple_word_count.py
@@ -1,8 +1,18 @@
 # simple testing word count in a string
-print('Enter your text')
-string = input()
-words = string.split()
-total = 0
-for words in words:
-    total = total + 1
-print("The total number of words in example sentence is ", total)
+con = True
+while con is True:
+    print('Enter your text')
+    string = input()
+    words = string.split()
+    total = 0
+    for words in words:
+        total = total + 1
+    print("The total number of words in example sentence is ", total)
+    ans = "lol"
+    while ans != "y" and ans != "n":
+        print("Do you wish to try again?(Y/N)")
+        ans = str.lower(input())
+        if ans == "n":
+            con = False
+else:
+    print("Goodbye!")
